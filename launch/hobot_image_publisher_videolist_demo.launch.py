bb21bea0432e2b5e6da34bd54e98bf3091f2d180
zhuo.wang <zhuo.wang@horizon.ai>
2023-04-26 19:59:27 +0800

    Update package.xml, launch script for package split

--- launch/hobot_image_publisher_videolist_demo.launch.py
+++ launch/hobot_image_publisher_videolist_demo.launch.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2022，Horizon Robotics.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 
 from launch import LaunchDescription
@@ -8,17 +22,9 @@ from launch.substitutions import LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python import get_package_share_directory
 
-def generate_launch_description():
-    web_service_launch_include = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(
-                get_package_share_directory('websocket'),
-                'launch/hobot_websocket_service.launch.py'))
-    )
 
+def generate_launch_description():
     return LaunchDescription([
-        #启动webservice
-        web_service_launch_include,
         # 启动图片发布pkg
         Node(
             package='hobot_image_publisher',
@@ -30,58 +36,49 @@ def generate_launch_description():
                 # image_format为支持发布的本地视频格式，当前支持的格式有h264/h265/mp4
                 {"image_format": "mp4"},
                 {"msg_pub_topic_name": "/hbmem_img"},
-                {"fps": 30},# 优先使用从本地视频文件中解析出来的帧率，解析失败时会采用此fps
-                {"is_loop": True},# 是否循环发布  True/False
-                {"is_shared_mem": True}# 是否使用share_mem的方式通信, True/False
+                {"fps": 30},  # 优先使用从本地视频文件中解析出来的帧率，解析失败时会采用此fps
+                {"is_loop": True},  # 是否循环发布  True/False
+                {"is_shared_mem": True}  # 是否使用share_mem的方式通信, True/False
             ],
             arguments=['--ros-args', '--log-level', 'error']
         ),
         # 启动nv12图片解码&发布pkg
-        Node(
-            package='hobot_codec',
-            executable='hobot_codec_republish',
-            output='screen',
-            parameters=[
-                #处理通道号 取值0~3
-                {"channel": 1},
-                #接入数据传输的方式 ros/shared_mem
-                {"in_mode": "shared_mem"},
-                #订阅的数据格式, 其中h264/h265为视频回灌会订阅到的格式
-                {"in_format": "h264"},
-                {"out_mode": "ros"},
-                # 解码后的图片格式，订阅h264/h265格式，解码后的格式只能为bgr8/rgb8/nv12
-                {"out_format": "nv12"},
-                {"sub_topic": "/hbmem_img"},
-                {"pub_topic": "/image_nv12"}
-            ],
-            arguments=['--ros-args', '--log-level', 'error']
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(
+                    get_package_share_directory('hobot_codec'),
+                    'launch/hobot_codec_decode.launch.py')),
+            launch_arguments={
+                'codec_in_mode': 'shared_mem',
+                'codec_in_format': 'h264',
+                'codec_out_mode': 'ros',
+                'codec_out_format': 'nv12',
+                'codec_sub_topic': '/hbmem_img',
+                'codec_pub_topic': '/image_nv12'
+            }.items()
         ),
         # 启动jpeg图片编码&发布pkg
-        Node(
-            package='hobot_codec',
-            executable='hobot_codec_republish',
-            output='screen',
-            parameters=[
-                {"channel": 1},
-                {"in_mode": "ros"},
-                {"in_format": "nv12"},
-                {"out_mode": "ros"},
-                {"out_format": "jpeg"},
-                {"sub_topic": "/image_nv12"},
-                {"pub_topic": "/image_jpeg"}
-            ],
-            arguments=['--ros-args', '--log-level', 'error']
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(
+                    get_package_share_directory('hobot_codec'),
+                    'launch/hobot_codec_encode.launch.py')),
+            launch_arguments={
+                'codec_in_mode': 'ros',
+                'codec_out_mode': 'ros',
+                'codec_sub_topic': '/image_nv12',
+                'codec_pub_topic': '/image_jpeg'
+            }.items()
         ),
         # 启动web展示pkg
-        Node(
-            package='websocket',
-            executable='websocket',
-            output='screen',
-            parameters=[
-                {"image_topic": "/image_jpeg"},
-                {"image_type": "mjpeg"},
-                {"only_show_image": True},
-            ],
-            arguments=['--ros-args', '--log-level', 'error']
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(
+                    get_package_share_directory('websocket'),
+                    'launch/websocket.launch.py')),
+            launch_arguments={
+                'websocket_image_topic': '/image_jpeg',
+                'websocket_only_show_image': 'True'
+            }.items()
         )
     ])
